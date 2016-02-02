5db6d93f2a8d2b24103a99b524f33bca9afaa742
cjyueh <cjyueh@gmail.com>
2016-02-02 13:58:40 -0800

    creates game code files

---
+++ app.js
@@ -0,0 +1,40 @@
+// Connect-the-Dots Game boilerplate pseudo-code
+
+// Cool name for game: ??
+
+
+// GAME LOGIC / RULES OF PLAY
+// HTML/CSS has full image, dots and connections lines in separate tags
+// Upon page load, show only numbered dots, lines invisible
+// Click starter dotOne and 2 things happen:
+// 1 - countdown/timer starts
+// 2 - make connection line visible by clicking dotTwo
+// Dots glow as cursor hovers over them
+// Continue making connection lines visible by clicking dots in numbered order until finish back at dotOne
+// If timer reaches zero, stop game, reset image to dots for additional attempts
+
+
+function setImage() {
+  // sets only dots visible / lines invisible
+}
+
+function clickDot() {
+  // on dotOne click {
+  //   countdown();
+  // }
+  // on dotTwo and other clicks {
+  //   showConnect();
+  // }
+  // .hover(dot glows);
+}
+
+function countdown() {
+  // timer countdown
+  // if timer reaches zero {
+  //   setImage();
+  // }
+}
+
+function showConnect() {
+  // make connection lines visible
+}
