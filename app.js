49dbfbf180d15aac35a51f8e5192a1cbea445a13
cjyueh <cjyueh@gmail.com>
2016-02-04 14:24:30 -0800

    creates dotGlow function for all dots

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ Upon page load, show only numbered dots, connections lines hidden, dot-1 glows (
 When each dot is clicked:
   1 - line from previous dot is drawn to current dot
   2 - next dot in sequence starts to glow
-  3 - only next dot can be clicked
+  3 - only next dot can be clicked, current/previous dot(s) can't be re-clicked
   Special:
     a. when dot-1 is clicked - countdown starts
     b. when dot-34 is clicked - countdown stops (you win!)
@@ -37,7 +37,13 @@ function clickDot() {
   // $('#dot-4').on('click', lineConnect3to4); 
 
   var n = this.innerHTML;
-  $("#"+dotsArray[n]).on('click', dotGlow(n));
+  dotGlow(n);
+  // $(".dot").click(function() {
+  //   $(this).toggleClass('glow');
+  //   console.log($(this).next().next());
+  //   $(this).next().next().toggleClass('glow');
+  // });
+  // $("#"+dotsArray[n]).on('click', dotGlow);
 
   lineConnect(n);
 
@@ -47,7 +53,6 @@ function clickDot() {
   // on dot2 and other clicks {
   //   showConnect();
   // }
-  // .hover(dot glows);
 }
 
 /*
@@ -67,9 +72,9 @@ function oneDotActive(event) {
 
 var dotsArray = ["dot-0", "dot-1", "dot-2", "dot-3", "dot-4", "dot-5", "dot-6", "dot-7", "dot-8", "dot-9", "dot-10", "dot-11", "dot-12", "dot-13", "dot-14", "dot-15", "dot-16", "dot-17", "dot-18", "dot-19", "dot-20", "dot-21", "dot-22", "dot-23", "dot-24", "dot-25", "dot-26", "dot-27", "dot-28", "dot-29", "dot-30", "dot-31", "dot-32", "dot-33", "dot-34"];
 var lineWidthsArr = ["0", "+=105px", "0", "+=75", "0", "+=50px", "0", "+=35px", "+=35px", "0", "+=25px", "0", "+=20px", "0", "+=80px", "0", "+=15px", "0", "+=145px", "0", "+=40px", "0", "+=15px", "0", "+=40px", "0", "+=30px", "0", "+=20px", "0", "+=23px", "+=23px", "0", "+=105px"];
-var lineHeightsArr = ["0", "0", "+=125px", "0", "+=100px", "0", "+=80", "0", "0", "+=50px", "0", "+=60px", "0", "+=60px", "0", "+=55px", "0", "+=55px", "0", "+=81px", "0", "+=50px", "0", "+=135px", "0", "+=80px", "0", "+=130px", "0", "+=20px", "0", "0", "+=70px", "0"];
+var lineHeightsArr = ["0", "0", "+=125px", "0", "+=80px", "0", "+=80", "0", "0", "+=50px", "0", "+=60px", "0", "+=60px", "0", "+=55px", "0", "+=55px", "0", "+=81px", "0", "+=50px", "0", "+=135px", "0", "+=80px", "0", "+=130px", "0", "+=20px", "0", "0", "+=70px", "0"];
 
-function lineConnect(n) {
+function lineConnect() {
   $("#"+dotsArray[n]).animate({
     width: lineWidthsArr[n],
   });
@@ -99,10 +104,14 @@ function lineConnect3to4() {
   $('#dot-1').removeClass('glow');
 }
 
-function dotGlow(n) {
-  $("#"+dotsArray[n]).toggleClass('glow');
-  $('#'+dotsArray[n+1]).toggleClass('glow');
+function dotGlow() {
+  $(".dot").click(function() {
+    $(this).toggleClass('glow');
+    console.log($(this).next().next());
+    $(this).next().next().toggleClass('glow');
+  });
 }
+
 // function dotGlow1to2() {
 //   $('#dot-1').toggleClass('glow');
 //   $('#dot-2').toggleClass('glow');
@@ -130,5 +139,4 @@ function countdown() {
     }
     // if finish connecting dots, stop clock
   }, 1000);
-
 }
