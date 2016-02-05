7eec792a2cbb4657c98443987d7410dfa864a974
cjyueh <cjyueh@gmail.com>
2016-02-04 19:42:31 -0800

    animates orthogonal lines

--- app.js
+++ app.js
@@ -14,119 +14,63 @@ If timer reaches zero, stop game, reset image to dots for additional attempts
 
 $(document).ready(function() {
   console.log("linked.");
-  clickDot();
+  // dotGlow($('#dot-1'));
+  $(".dot").click(dotGlow);
+  $(".dot").click(function() {
+    var m = $(this);
+    lineConnect(m, lineWidthsArr, lineHeightsArr);
+  });
+  $('#dot-1').click(countdown);
 });
+
+  
+ 
 
 // function init() {
 //   //calls other functions
 // }
 
-// function setImage() {
-//   // sets only dots visible / lines invisible
-//   // set dot1 effect
-// }
-
-function clickDot() {
-  // set event listener on a set of jquery elements
-  // $(".dot").on("click", function(e){ console.log(e.target, "clicked")})
-  // $('#dot-1').on('click', dotGlow1to2);
-  // $('#dot-2').on('click', lineConnect1to2);
-  // $('#dot-2').on('click', dotGlow2to3);
-  // $('#dot-3').on('click', lineConnect2to3);
-  // $('#dot-3').on('click', dotGlow3to4);
-  // $('#dot-4').on('click', lineConnect3to4); 
-
-  var n = this.innerHTML;
-  dotGlow(n);
-  // $(".dot").click(function() {
-  //   $(this).toggleClass('glow');
-  //   console.log($(this).next().next());
-  //   $(this).next().next().toggleClass('glow');
-  // });
-  // $("#"+dotsArray[n]).on('click', dotGlow);
-
-  lineConnect(n);
-
-  // on dot1 click {
-  //   countdown();
-  // }
-  // on dot2 and other clicks {
-  //   showConnect();
-  // }
-}
-
-/*
-  event callback functions get passed the event object
-  use the `.target` method on the event to find the element that the event
-  just occurred on
-*/
-
-function oneDotActive(event) {
-  var dots = document.getElementsByClassName("dot");
-  var listener = function() {
-    // something;
-  };
-  event.target.addEventListener('click', listener, false);
-  event.target.removeEventListener('click', listener, false);
+function resetDots() {
+  $('#dot-1').toggleClass('glow'); //resets dot-1 glow
+  // lines invisible
 }
 
 var dotsArray = ["dot-0", "dot-1", "dot-2", "dot-3", "dot-4", "dot-5", "dot-6", "dot-7", "dot-8", "dot-9", "dot-10", "dot-11", "dot-12", "dot-13", "dot-14", "dot-15", "dot-16", "dot-17", "dot-18", "dot-19", "dot-20", "dot-21", "dot-22", "dot-23", "dot-24", "dot-25", "dot-26", "dot-27", "dot-28", "dot-29", "dot-30", "dot-31", "dot-32", "dot-33", "dot-34"];
 var lineWidthsArr = ["0", "+=105px", "0", "+=75", "0", "+=50px", "0", "+=35px", "+=35px", "0", "+=25px", "0", "+=20px", "0", "+=80px", "0", "+=15px", "0", "+=145px", "0", "+=40px", "0", "+=15px", "0", "+=40px", "0", "+=30px", "0", "+=20px", "0", "+=23px", "+=23px", "0", "+=105px"];
-var lineHeightsArr = ["0", "0", "+=125px", "0", "+=80px", "0", "+=80", "0", "0", "+=50px", "0", "+=60px", "0", "+=60px", "0", "+=55px", "0", "+=55px", "0", "+=81px", "0", "+=50px", "0", "+=135px", "0", "+=80px", "0", "+=130px", "0", "+=20px", "0", "0", "+=70px", "0"];
+var lineHeightsArr = ["0", "0", "+=125px", "0", "+=80px", "0", "+=80", "0", "0", "+=30px", "0", "+=60px", "0", "+=60px", "0", "+=55px", "0", "+=55px", "0", "+=81px", "0", "+=50px", "0", "+=135px", "0", "+=80px", "0", "+=130px", "0", "+=20px", "0", "0", "+=70px", "0"];
 
-function lineConnect() {
-  $("#"+dotsArray[n]).animate({
-    width: lineWidthsArr[n],
-  });
-  $("#"+dotsArray[n]).animate({
-    height: lineHeightsArr[n],
-  });
-}
-
-function lineConnect1to2() {
-  $('#line-1').animate({
-      width: "+=105px"
-  });
-  console.log(this);
-  console.log(this.innerHTML);
-}
-
-function lineConnect2to3() {
-  $('#line-2').animate({
-      height: "+=125px"
-  });
-}
-
-function lineConnect3to4() {
-  $('#line-3').animate({
-    width: "+=75px",
-  });
-  $('#dot-1').removeClass('glow');
+function lineConnect(m, lineWidthsArr, lineHeightsArr) {
+  var n = m.html();
+  // console.log(n);
+  // if 'this' = dot-1 is clicked, no lines drawn
+  // if 'this' = other dots are clicked (n>1), draw lines from previous dot
+      //if line is ".horizontal" || ".diagonal", width: lineWidthsArr[n];
+      //if line is ".vertical", height: lineHeightsArr[n];
+  if (n === 1) {
+    countdown();
+  } else if (n > 1) {
+    if ($('#line-'+(n-1)).hasClass("horizontal" || "diagonal")) {
+      var lineWidth = lineWidthsArr[(n-1)];
+      console.log(lineWidth);
+      $('#line-'+(n-1)).animate({
+      width: lineWidth,
+      });
+    } else {
+      var lineHeight = lineHeightsArr[(n-1)];
+      // console.log(lineHeight);
+      $('#line-'+(n-1)).animate({
+      height: lineHeight,
+      });
+    }
+  }
 }
 
 function dotGlow() {
-  $(".dot").click(function() {
-    $(this).toggleClass('glow');
-    console.log($(this).next().next());
-    $(this).next().next().toggleClass('glow');
-  });
+  // console.log($(this));
+  $(this).toggleClass('glow'); //turn off glow of currently clicked dot
+  // console.log($(this).next().next());
+  $(this).next().next().toggleClass('glow'); //turn on glow of next dot  
 }
-
-// function dotGlow1to2() {
-//   $('#dot-1').toggleClass('glow');
-//   $('#dot-2').toggleClass('glow');
-//   countdown();
-// }
-
-// function dotGlow2to3() {
-//   $('#dot-2').toggleClass('glow');
-//   $('#dot-3').toggleClass('glow');
-// }
-
-// function dotGlow3to4() {
-//   $('#dot-3').toggleClass('glow');
-//   $('#dot-4').toggleClass('glow');
-// }
 
 function countdown() {
   var seconds = 15;
